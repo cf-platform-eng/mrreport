acccf91628b88ee342cfc9d14221aa0b51246a69
Todd Ritchie <tritchie+github@pivotal.io>
2019-07-18 15:22:29 -0400

    First pass at specs around parseLogs
    Co-authored-by: Pete Wall <pwall@pivotal.io>

--- html/presenter.js
+++ html/presenter.js
@@ -1,3 +1,28 @@
-const logData = document.getElementById('logData').innerHTML
-const display = document.getElementById('display')
-display.innerHTML = logData
+const presenter = {
+    injectElements: () => {
+        const logData = document.getElementById('logData').innerHTML
+        const display = document.getElementById('display')
+        display.innerHTML = logData;
+    },
+
+    parseLogData: (input) => {
+        let regex = /section-start: '(.+)' MRL:({.+})((.|\n)*)section-end: '(\1)' result: (\d+) MRL:({.+})/gm;
+        let matches = [];
+        let m;
+
+        while ((m = regex.exec(input)) !== null) {
+            var name = m[1];
+            var startMrl = m[2];
+            var contents = m[3];
+            var statusCode = m[6];
+            var endMrl = m[7];
+            var sections = presenter.parseLogData(contents);
+        
+            matches.push({ name, startMrl, contents, sections, statusCode, endMrl });
+        }
+
+        return matches;
+    }
+};
+
+module.exports = presenter;
--- html/spec/presenterSpec.js
+++ html/spec/presenterSpec.js
@@ -1,28 +1,59 @@
+const fs = require("fs");
+const { promisify } = require("util");
+const readFile = promisify(fs.readFile);
+
 document = {
     getElementById: (elementId) => {}
-}
+};
 
 describe("the presenter", () => {
-    let logDataElement = {}
+    let logDataElement = {};
     let displayElement = {
         get innerHTML() {},
         set innerHTML(v) {},
-    }
+    };
 
-    let getElementById
-    let displayInnerHTML
+    let getElementById;
+    let displayInnerHTML;
+    let presenter = require("../presenter.js");
 
     beforeEach(() => {
-        displayInnerHTML = spyOnProperty(displayElement, "innerHTML", "set")
-        getElementById = spyOn(document, 'getElementById').and.returnValues(logDataElement, displayElement)
-    })
+        displayInnerHTML = spyOnProperty(displayElement, "innerHTML", "set");
+        getElementById = spyOn(document, 'getElementById').and.returnValues(logDataElement, displayElement);
+    });
 
     it("populates the display with the log data", () => {
-        logDataElement.innerHTML = "this is my log"
+        logDataElement.innerHTML = "this is my log";
 
-        require("../presenter.js")
-        expect(getElementById).toHaveBeenCalledWith("logData")
-        expect(getElementById).toHaveBeenCalledWith("display")
-        expect(displayInnerHTML).toHaveBeenCalledWith("this is my log")
-    })
-})
+        presenter.injectElements();
+
+        expect(getElementById).toHaveBeenCalledWith("logData");
+        expect(getElementById).toHaveBeenCalledWith("display");
+        expect(displayInnerHTML).toHaveBeenCalledWith("this is my log");
+    });
+
+    describe("when there are sections", () => {
+        let rawLogData;
+        beforeEach(async () => {
+            rawLogData = await readFile('./spec/support/fixtures/flat_markers.log');
+        });
+
+        it("splits based on tags into array of objects", () => {
+            let parsed = presenter.parseLogData(rawLogData);
+            expect(parsed.length).toBe(3);
+        });
+    });
+
+    describe("when there are nested sections", () => {
+        let rawLogData;
+        beforeEach(async () => {
+            rawLogData = await readFile('./spec/support/fixtures/nested_markers.log');
+        });
+
+        it("returns nested arrays of objects", () => {
+            let parsed = presenter.parseLogData(rawLogData);
+            expect(parsed.length).toBe(2);
+            expect(parsed[0].sections.length).toBe(1);
+        });
+    });
+});
